27ba4d39b81453301d7c364be53fe4ef6bbf3844
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-25 23:10:16 +0330

    Decorator 1

---
+++ Decorator/Decorator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class Decorator : Component
+    {
+        private readonly Component _component;
+        public Decorator(Component component)
+        {
+            _component = component;
+        }
+
+        public override void operation()
+        {
+            _component.operation();
+        }
+    }
+
+    public class ConcreteDecorator : Decorator
+    {
+        public ConcreteDecorator(Component component) : base(component)
+        {
+        }
+
+        public override void operation()
+        {
+            base.operation();
+            NewFeature();
+        }
+
+        public void NewFeature()
+        {
+            Console.WriteLine("New Feature");
+        }
+    }
+
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var concrete = new ConcreteComponent();
+
+            //concrete.operation();
+            var concreteDecorator = new ConcreteDecorator(concrete);
+            concreteDecorator.operation();
+        }
+    }
+}
